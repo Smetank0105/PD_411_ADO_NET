23046c60810116bf90a9fad0a8471ed49c0998fd
Smetank0105 <206859408+Smetank0105@users.noreply.github.com>
2025-09-15 16:57:35 +0700

    Add project files.

---
+++ ADO_NET/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//ADO.NET
+using System.Data.SqlClient;
+
+namespace ADO_NET
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello ADO.NET");
+		}
+	}
+}
