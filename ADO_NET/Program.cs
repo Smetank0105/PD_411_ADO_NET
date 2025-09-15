cf71de4f1127cc4c2cc898f7999c21ecf1dccaa7
Smetank0105 <206859408+Smetank0105@users.noreply.github.com>
2025-09-15 19:20:15 +0700

    Reader start

--- ADO_NET/Program.cs
+++ ADO_NET/Program.cs
@@ -12,7 +12,37 @@ namespace ADO_NET
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello ADO.NET");
+			//1) Создаем подключчение к Базе Данны на Сервере
+			string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Movies;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+			Console.WriteLine(connectionString);
+			SqlConnection connection = new SqlConnection();
+			connection.ConnectionString = connectionString;
+			//2) Открываем соединение
+			//После того, как подключение создано, оно не является открытым. т.е. подключение всегда открывается в ручную при необходимости
+			connection.Open();
+
+			//3) Создаем 'DataReader'
+			string cmd = "SELECT * FROM Directors";
+			SqlCommand command = new SqlCommand(cmd, connection);
+
+			//4) Создаем 'Reader'
+			SqlDataReader reader = command.ExecuteReader();
+			for (int i = 0; i < reader.FieldCount; i++)
+			{
+				Console.Write(reader.GetName(i) + "\t");
+			}
+			Console.WriteLine();
+			while (reader.Read())
+			{
+				//Console.WriteLine($"{reader[0]}\t{reader[1]}\t{reader[2]}");
+				for(int i=0; i < reader.FieldCount;i++)
+					Console.Write(reader[i]+"\t\t");
+				Console.WriteLine();
+			}
+			reader.Close();
+
+			//?) Подключение обязательно нужно закрывать
+			connection.Close();
 		}
 	}
 }
